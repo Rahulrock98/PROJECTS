252bb16682a7aeac588e904d22b98d5a861da201
Rahul Kumar <72124534+Rahulrock98@users.noreply.github.com>
2024-07-14 11:04:25 +0000

    quiz

---
+++ Quiz/script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false }
+        ]
+    },
+    {
+        question: "Which country won the T20 World cup in 2024?",
+        answers: [
+            { text: "Australia", correct: false },
+            { text: "India", correct: true },
+            { text: "Afganistan", correct: false },
+            { text: "Sri Lanka", correct: false }
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "Australia", correct: false },
+            { text: "India", correct: true },
+            { text: "Afganistan", correct: false },
+            { text: "Sri Lanka", correct: false }
+        ]
+    }
+    
+]
